5d30e99868fa2a86e97183c16384dcd74708c8d8
Megumi <megumi67@gmail.com>
2020-05-13 23:21:18 +0900

    set up app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,22 +1,5 @@
 console.log('Client side JS is loaded')
 
-//fetch('http://puzzle.mead.io.puzzle').then((response) => {
-//  response.json().then((data) => {
-//    console.log(data)
-//  })
-//})
-
-//fetch('http://localhost:3000/weather?address=boston').then((response)=> {
-//  response.json().then((data) => {
-//    if ( data.error){
-//        console.log(data.error)
-//    }else{
-//        console.log(data.forecast)
-//    }
-//    console.log(data)
-//  })
-//})
-//
 const weatherForm =  document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1') 
@@ -31,7 +14,7 @@ weatherForm.addEventListener('submit', (e) => {
   messageOne.textContent = 'Loading...'
   messageTwo.textContent = ''
 
-  fetch('http://localhost:3000/weather?address=' + location).then((response)=> {
+  fetch('/weather?address=' + location).then((response)=> {
     response.json().then((data) => {
       if (data.error){
          return messageOne.textContent = data.error
